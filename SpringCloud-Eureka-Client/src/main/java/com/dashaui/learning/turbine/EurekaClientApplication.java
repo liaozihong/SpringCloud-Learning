e99a6a3eea028b2492e2aac5c41524a21edac6d1
zihong <15017263266@163.com>
2018-12-05 11:19:09 +0800

    fixed:去除客户端的安全模块

--- SpringCloud-Eureka-Client/src/main/java/com/dashaui/learning/turbine/EurekaClientApplication.java
+++ SpringCloud-Eureka-Client/src/main/java/com/dashaui/learning/turbine/EurekaClientApplication.java
@@ -9,9 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -44,15 +41,4 @@ public class EurekaClientApplication {
         return "hi,"+name+",sorry,error!";
     }
 
-    /**
-     * 配置授权，用于监控
-     */
-    @Configuration
-    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-        @Override
-        protected void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().permitAll()
-                    .and().csrf().disable();
-        }
-    }
 }
